5802a5317e5e1ee43e969b6934b8d65827e76b96
Josh Sinclair <joshsinclair@Joshs-MacBook-Pro.local>
2017-01-12 10:59:50 +1100

    debugging

--- django_celery_beat/models.py
+++ django_celery_beat/models.py
@@ -404,6 +404,7 @@ class StartDateSchedule(BaseSchedule):
         )
         
     def is_due(self, last_run_at):
+        now = datetime.datetime.now(tz=pytz.utc)
         # last run at isnt a good indication of whether to run the first time or not
         if now >= self.start_date and (last_run_at < now or last_run_at is None):
             print ("first if")
