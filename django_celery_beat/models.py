f1c838bb62771d7f444e3a6a21f789af46e41d1d
Josh Sinclair <joshsinclair@Joshs-MacBook-Pro.local>
2017-01-12 11:11:24 +1100

    debugging

--- django_celery_beat/models.py
+++ django_celery_beat/models.py
@@ -406,10 +406,10 @@ class StartDateSchedule(BaseSchedule):
     def is_due(self, last_run_at):
         now = datetime.datetime.now(tz=pytz.utc)
         # last run at isnt a good indication of whether to run the first time or not
-        if now >= self.start_date and (last_run_at < now or last_run_at is None):
+        if now >= self.start_date and (pytz.utc.localize(last_run_at) < now or last_run_at is None):
             print ("first if")
             return schedstate(is_due=True, next=self.seconds)
-        elif now >= self.start_date and self.last_run_at > now:
+        elif now >= self.start_date and pytz.utc.localize(last_run_at) > now:
             print ("second if")
             last_run_at = self.maybe_make_aware(last_run_at)
             rem_delta = self.remaining_estimate(last_run_at)
